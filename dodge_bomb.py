078f76f78009d63440e79a311d30d371d47c98a8
c0a22135 <c0a2213515@edu.teu.ac.jp>
2023-10-03 15:17:22 +0900

    練習問題3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,7 +2,17 @@ import sys
 import pygame as pg
 import random
 
-WIDTH, HEIGHT = 1100, 600
+WIDTH, HEIGHT = 1200, 600
+BOMB_RADIUS = 10
+BOMB_COLOR = (255, 0, 0)  # 赤色
+vx = 5
+vy = 5
+KEY_MOVEMENTS = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, 5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (5, 0)
+}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -10,45 +20,40 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-
-    # 爆弾Surfaceを作成
-    bomb_radius = 10
-    bomb_color = (255, 0, 0)
-    bomb_img = pg.Surface((2 * bomb_radius, 2 * bomb_radius), pg.SRCALPHA)
-    pg.draw.circle(bomb_img, bomb_color, (bomb_radius, bomb_radius), bomb_radius)
-    bomb_img.set_colorkey((0, 0, 0))
-
+    kk_rect = kk_img.get_rect(topleft=(900, 400))
     clock = pg.time.Clock()
     tmr = 0
 
-    bomb_x = random.randint(0, WIDTH - 2 * bomb_radius)
-    bomb_y = random.randint(0, HEIGHT - 2 * bomb_radius)
-    vx, vy = 5, 5
+    # 爆弾Surfaceの作成
+    bomb_surface = pg.Surface((BOMB_RADIUS * 2, BOMB_RADIUS * 2))
+    bomb_surface.fill((0, 0, 0))
+    pg.draw.circle(bomb_surface, BOMB_COLOR, (BOMB_RADIUS, BOMB_RADIUS), BOMB_RADIUS)
+    bomb_surface.set_colorkey((0, 0, 0))
+    bomb_rect = bomb_surface.get_rect()
+    bomb_rect.x = random.randint(0, WIDTH - BOMB_RADIUS * 2)
+    bomb_rect.y = random.randint(0, HEIGHT - BOMB_RADIUS * 2)
 
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT:
+            if event.type == pg.QUIT: 
                 return
 
+        key_lst = pg.key.get_pressed()
+        dx, dy = 0, 0
+        for key, (vx, vy) in KEY_MOVEMENTS.items():
+            if key_lst[key]:
+                dx += vx
+                dy += vy
+        kk_rect.move_ip(dx, dy)
+
+        bomb_rect.move_ip(vx, vy)  # 爆弾の位置を移動
+
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
-
-        # 爆弾を表示
-        screen.blit(bomb_img, (bomb_x, bomb_y))
-
-        # 爆弾を移動
-        bomb_x += vx
-        bomb_y += vy
-
-        # 画面端で跳ね返る
-        if bomb_x <= 0 or bomb_x >= WIDTH - 2 * bomb_radius:
-            vx *= -1
-        if bomb_y <= 0 or bomb_y >= HEIGHT - 2 * bomb_radius:
-            vy *= -1
-
+        screen.blit(kk_img, kk_rect.topleft)
+        screen.blit(bomb_surface, bomb_rect.topleft)  # 爆弾の表示
         pg.display.update()
         tmr += 1
-        clock.tick(100)
+        clock.tick(50)  # FPSを50に変更
 
 if __name__ == "__main__":
     pg.init()
